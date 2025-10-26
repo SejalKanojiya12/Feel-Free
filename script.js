5d21af3bd9489d71824680abbb6ffe57e1c0b738
SejalKanojiya12 <sejalkanojiya883@gmail.com>
2025-10-27 00:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const quotes = [
+    "Here, your silence finds a voice.",
+    "Every emotion deserves to be felt.",
+    "Healing begins the moment you share.",
+    "No judgments. No names. Just feelings.",
+    "Because being human is never a weakness."
+  ];
+
+  const quoteText = document.querySelector(".quote-text");
+  let index = 0;
+
+  setInterval(() => {
+    index = (index + 1) % quotes.length;
+    quoteText.textContent = quotes[index];
+  }, 6000);
+});
